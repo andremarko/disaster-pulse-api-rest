0b649701daff3cdb522e9a494bec562623ce25d1
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-27 21:57:05 -0300

    first commit

---
+++ src/test/java/disaster/pulse/api/DisasterPulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package disaster.pulse.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterPulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
