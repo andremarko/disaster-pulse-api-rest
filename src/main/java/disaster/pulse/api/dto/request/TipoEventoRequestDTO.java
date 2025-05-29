1d084c63d6bcca1b1359e644dd7cb25b2d45619b
andremarko <151890840+andremarko@users.noreply.github.com>
2025-05-28 23:45:48 -0300

    refactor(dto): renomeia TipoEvento para nomenclatura padrão

--- src/main/java/disaster/pulse/api/dto/request/TipoEventoRequestDTO.java
+++ src/main/java/disaster/pulse/api/dto/request/TipoEventoRequestDTO.java
@@ -3,7 +3,7 @@ package disaster.pulse.api.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record TipoEvento(
+public record TipoEventoRequestDTO(
         @Size(max=30, message = "Máximo de 30 caracteres")
         @NotBlank
         String tipo
